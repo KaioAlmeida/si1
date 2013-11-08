da6667b410a2b3dafe86aa47f7b63cae1f6e034e
Kaio Almeida <kaiomaxalmeida@gmail.com>
2013-11-08 15:27:20 -0200

    Refatoramento do codigo e add comentarios.

--- lab1/src/code/NumbersInWords.java
+++ lab1/src/code/NumbersInWords.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package code;
 
 import java.util.Scanner;
@@ -23,7 +20,40 @@ public class NumbersInWords {
 		centenas = new String[] {"cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"};		
 	}
 	
-	public String NumberToWords(int num) {
+	/**
+	 * Recebe um número e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return String com a escreita do numero por extenso
+	 */
+	public String NumberToWords(int num) {						
+		String numS = Integer.toString(num);
+		int numDigitos = numS.length();				
+		if (numDigitos < 4) {
+			return centena (num);
+		}else if (numDigitos == 4) {
+			return milhar(num);
+		}else if (numDigitos == 5) {
+			return dezenaDeMilhar(num);
+		}else if (numDigitos == 6) {
+			return centenaDeMilhar(num);
+		}else if (numDigitos == 7) {
+			return milhoes(num);
+		}else if(numDigitos == 8) {
+			return dezenasDemilhoes(num);		
+		}else if (numDigitos == 9) {
+			return centenasDemilhoes(num);
+		}else if(num == 1000000000){//1 bilhao
+			return "um bilhão";
+		}				
+	return "Numero invalido";		
+	}
+	
+	/**
+	 * Recebe um número no intervalo(0-999) e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String centena (int num) {
 		if (num < 0) { // numeros menores que 0
 			return "Numero invalido";
 		}		
@@ -48,36 +78,19 @@ public class NumbersInWords {
 			}else if (num % 10 == 0) { // numeros entre 120 a 990 divisiveis por 10
 				return centenas[(num/100) - 1] + " e " + dezenas[((num % 100)/10) -2];
 			} // o restante dos numeros
-			return centenas[(num/100) - 1] + " e " + dezenas[((num % 100)/10) -2] + " e " + unidades[(num % 100) % 10];	
-		}else {
-			if (num == 1000) {//mil
-				return "mil";
-			}else if(num == 1000000){//1 milhao	
-				return "um milhão";
-			}else if(num == 1000000000){//1 bilhao
-				return "um bilhão";
-			}else {// numeros entre 1.001 e 1 bilhao
-				String numS = Integer.toString(num);
-				int numDigitos = numS.length();
-				if (numDigitos == 4) {
-					return milhar(num);
-				}else if (numDigitos == 5) {
-					return dezenaDeMilhar(num);
-				}else if (numDigitos == 6) {
-					return centenaDeMilhar(num);
-				}else if (numDigitos == 7) {
-					return milhoes(num);
-				}else if(numDigitos == 8) {
-					return dezenasDemilhoes(num);		
-				}else if (numDigitos == 9) {
-					return centenasDemilhoes(num);
-				}				
-			}
-			return "Numero invalido";
 		}
+		return centenas[(num/100) - 1] + " e " + dezenas[((num % 100)/10) -2] + " e " + unidades[(num % 100) % 10];		
 	}
 	
-	public String milhar(int num) {
+	/**
+	 * Recebe um número de 4 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+ 	private String milhar(int num) {
+		if (num == 1000) {//mil
+			return "mil";
+		}
 		int milhares = Integer.parseInt(Integer.toString(num).substring(0, 1));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(1, 4));
 		if (milhares == 1) {
@@ -93,7 +106,12 @@ public class NumbersInWords {
 			}		
 	}
 	
-	public String dezenaDeMilhar(int num) {
+ 	/**
+	 * Recebe um número de 5 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String dezenaDeMilhar(int num) {
 		int milhares = Integer.parseInt(Integer.toString(num).substring(0, 2));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(2, 5));
 		if (centenas == 0) {
@@ -103,8 +121,13 @@ public class NumbersInWords {
 		}					
 		return NumberToWords(milhares) + " mil " + NumberToWords(centenas);			
 	}
-	
-	public String centenaDeMilhar(int num) {
+		
+	/**
+	 * Recebe um número de 6 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String centenaDeMilhar(int num) {
 		int milhares = Integer.parseInt(Integer.toString(num).substring(0, 3));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(3, 6));
 		if (centenas == 0) {
@@ -114,71 +137,54 @@ public class NumbersInWords {
 		}					
 		return NumberToWords(milhares) + " mil " + NumberToWords(centenas);		
 	}
-		
-	public String milhoes(int num) {
+	
+	/**
+	 * Recebe um número de 7 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String milhoes(int num) {
+		if(num == 1000000){//1 milhao	
+			return "um milhão";
+		}
 		int milhoes = Integer.parseInt(Integer.toString(num).substring(0, 1));	
 		int milhares = Integer.parseInt(Integer.toString(num).substring(1, 4));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(4, 7));
-		String milhoess;
-		if (milhoes > 1) {
-			milhoess = " milhões ";
-		}else {
-			milhoess = " milhão ";
-		}
-		if (centenas == 0 && milhares == 0) {
-			return NumberToWords(milhoes) + " milhões";
-		}else if (milhares == 0) {
-			if (centenas <= 100) {
-				return NumberToWords(milhoes) + milhoess + "e " + NumberToWords(centenas);
-			}						
-			return NumberToWords(milhoes) + milhoess + NumberToWords(centenas);
-		}else if (centenas == 0) {
-			if (milhares < 100) {
-				return NumberToWords(milhoes) + milhoess + "e " + NumberToWords(milhares) + " mil";
-			}
-			return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil";
-		}else {
-			if (centenas <= 100) {
-				return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil e " + NumberToWords(centenas);
-			}						
-			return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil " + NumberToWords(centenas);
-		}
+		return writeMilhoes(centenas, milhares, milhoes);
 	}
-	
-	public String dezenasDemilhoes(int num) {
+		
+	/**
+	 * Recebe um número de 8 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String dezenasDemilhoes(int num) {
 		int milhoes = Integer.parseInt(Integer.toString(num).substring(0, 2));	
 		int milhares = Integer.parseInt(Integer.toString(num).substring(2, 5));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(5, 8));
-		String milhoess;
-		if (milhoes > 1) {
-			milhoess = " milhões ";
-		}else {
-			milhoess = " milhão ";
-		}
-		if (centenas == 0 && milhares == 0) {
-			return NumberToWords(milhoes) + " milhões";
-		}else if (milhares == 0) {
-			if (centenas <= 100) {
-				return NumberToWords(milhoes) + milhoess + "e " + NumberToWords(centenas);
-			}						
-			return NumberToWords(milhoes) + milhoess + NumberToWords(centenas);
-		}else if (centenas == 0) {
-			if (milhares < 100) {
-				return NumberToWords(milhoes) + milhoess + "e " + NumberToWords(milhares) + " mil";
-			}
-			return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil";
-		}else {
-			if (centenas <= 100) {
-				return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil e " + NumberToWords(centenas);
-			}						
-			return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil " + NumberToWords(centenas);
-		}
+		return writeMilhoes(centenas, milhares, milhoes);
 	}
 	
-	public String centenasDemilhoes(int num) {
+	/**
+	 * Recebe um número de 9 digitos e retorna sua escrita por extenso.
+	 * @param int num
+	 * @return O num escrito por extenso
+	 */
+	private String centenasDemilhoes(int num) {
 		int milhoes = Integer.parseInt(Integer.toString(num).substring(0, 3));	
 		int milhares = Integer.parseInt(Integer.toString(num).substring(3, 6));	
 		int centenas = Integer.parseInt(Integer.toString(num).substring(6, 9));
+		return writeMilhoes(centenas, milhares, milhoes);
+	}
+	
+	/**
+	 * Recebe as 3 unidades dos numeros de 7 a 9 digitos e aos retorna por extenso.
+	 * @param int centenas
+	 * @param int milhares
+	 * @param int milhoes
+	 * @return O num escrito por extenso
+	 */
+	private String writeMilhoes(int centenas, int milhares, int milhoes) {
 		String milhoess;
 		if (milhoes > 1) {
 			milhoess = " milhões ";
@@ -202,11 +208,12 @@ public class NumbersInWords {
 				return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil e " + NumberToWords(centenas);
 			}						
 			return NumberToWords(milhoes) + milhoess + NumberToWords(milhares) + " mil " + NumberToWords(centenas);
-		}
+		}		
 	}
 	
-		
+	
 	/**
+	 * main
 	 * Interface via linha de comando.
 	 * 
 	 * @param args
